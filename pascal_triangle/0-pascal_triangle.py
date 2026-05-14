9f74b367af61221cd7b103f529f3f4acc2bf786e
Jonathan <jonathan.cahoreau@gmail.com>
2026-05-14 14:49:25 +0200

    ajout de if

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -1,15 +1,18 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
 def pascal_triangle(n):
+    if n <= 0:
+        return []
+
     triangle = []
 
     for i in range(n):
         row = [1]
 
         if triangle:
-            last_row = triangle[-1]
+            last = triangle[-1]
 
-            for j in range(len(last_row) - 1):
-                row.append(last_row[j] + last_row[j + 1])
+            for j in range(len(last) - 1):
+                row.append(last[j] + last[j + 1])
 
             row.append(1)
 
