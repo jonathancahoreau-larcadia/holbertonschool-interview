e7cd93c2c8d4e32d595216a22c083aabad6937c3
Jonathan <jonathan.cahoreau@gmail.com>
2026-05-14 14:45:31 +0200

    gestion de la premiere ligne

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
 def pascal_triangle(n):
-    if n <= 0:
-        return []
-    triangle = [[1]]
-    for i in range(n - 1):
-        prev_list = triangle[-1]
-        new_list = [1]
-        for j in range(len(prev_list) - 1):
-            new_list.append(prev_list[j] + prev_list[j+1])
-        new_list.append(1)
-        triangle.append(new_list)
+    triangle = []
+
+    for i in range(n):
+        row = [1]
+
+        if triangle:
+            last_row = triangle[-1]
+
+            for j in range(len(last_row) - 1):
+                row.append(last_row[j] + last_row[j + 1])
+
+            row.append(1)
+
+        triangle.append(row)
+
     return triangle
