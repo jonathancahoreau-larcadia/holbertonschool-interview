83a3fe49d2d927a1d1acb8804352207fca1454f4
Jonathan <jonathan.cahoreau@gmail.com>
2026-05-14 15:00:41 +0200

    add documentation

--- pascal_triangle/0-pascal_triangle.py
+++ pascal_triangle/0-pascal_triangle.py
@@ -1,5 +1,24 @@
 #!/usr/bin/python3
+"""
+Module for generating Pascal's Triangle.
+
+This module provides a function to generate Pascal's Triangle
+up to a given number of rows.
+"""
+
+
 def pascal_triangle(n):
+    """
+    Generate Pascal's Triangle up to n rows.
+
+    Args:
+        n (int): The number of rows of Pascal's Triangle
+        to generate.
+
+    Returns:
+        list of lists: A list of lists of integers
+        representing Pascal's Triangle.
+    """
     result = []
 
     for i in range(n):
