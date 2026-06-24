ae043ea3b2eb5a84103d4197792d886b0d7b76f8
Jonathan <jonathan.cahoreau@gmail.com>
2026-06-24 09:21:25 +0200

    syntax changed

--- starwars_api/0-starwars_characters.js
+++ starwars_api/0-starwars_characters.js
@@ -1,28 +1,32 @@
 #!/usr/bin/node
 
-const request = require("request");
+const request = require('request');
 const movieId = process.argv[2];
+
 if (!movieId) process.exit(1);
 
-const filmUrl = `https://swapi.dev/api/films/${movieId}/`;
+const filmUrl = `https://swapi-api.hbtn.io/api/films/${movieId}/`;
 
-function getJSON(url) {
+function getJSON (url) {
   return new Promise((resolve, reject) => {
-    request(url, { json: true, strictSSL: false }, (err, res, body) => {
-      if (err) return reject(err);
-      if (res.statusCode !== 200)
-        return reject(new Error(`Status ${res.statusCode}`));
+    request(url, { json: true, strictSSL: false }, (error, response, body) => {
+      if (error) return reject(error);
+
+      if (response.statusCode !== 200) {
+        return reject(new Error(`Status ${response.statusCode}`));
+      }
+
       resolve(body);
     });
   });
 }
 
 getJSON(filmUrl)
-  .then((film) => {
-    const chars = film.characters || [];
-    return Promise.all(chars.map((url) => getJSON(url)));
+  .then(film => {
+    const characters = film.characters || [];
+    return Promise.all(characters.map(url => getJSON(url)));
   })
-  .then((charObjs) => {
-    charObjs.forEach((c) => console.log(c.name));
+  .then(characterObjects => {
+    characterObjects.forEach(character => console.log(character.name));
   })
-  .catch((err) => console.error(err));
+  .catch(error => console.error(error));
