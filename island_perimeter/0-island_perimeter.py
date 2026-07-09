3f0f881b384aa76d36929ada6e3115d73237cf75
Jonathan <jonathan.cahoreau@gmail.com>
2026-07-09 14:07:47 +0200

    fix indexerror

--- island_perimeter/0-island_perimeter.py
+++ island_perimeter/0-island_perimeter.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+"""Module that computes the perimeter of an island."""
 
 
 def island_perimeter(grid):
@@ -11,15 +12,15 @@ def island_perimeter(grid):
         The perimeter of the island.
     """
     perimeter = 0
-    for row in range(1, len(grid)):
-        for col in range(1, len(grid[row])):
+    for row in range(len(grid)):
+        for col in range(len(grid[row])):
             if grid[row][col] == 1:
-                if grid[row - 1][col] == 0:
+                if row == 0 or grid[row - 1][col] == 0:
                     perimeter += 1
-                if grid[row + 1][col] == 0:
+                if row == len(grid) - 1 or grid[row + 1][col] == 0:
                     perimeter += 1
-                if grid[row][col + 1] == 0:
+                if col == 0 or grid[row][col - 1] == 0:
                     perimeter += 1
-                if grid[row][col - 1] == 0:
+                if col == len(grid[row]) - 1 or grid[row][col + 1] == 0:
                     perimeter += 1
     return perimeter
